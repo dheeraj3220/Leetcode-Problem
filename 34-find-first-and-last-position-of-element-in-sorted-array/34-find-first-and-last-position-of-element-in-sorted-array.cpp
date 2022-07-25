a0b043a9522f5a0be940c91ca06839affe032bb4
dheeraj3220 <76546604+dheeraj3220@users.noreply.github.com>
2022-07-26 02:14:49 +0530

    Time: 15 ms (39.90%), Space: 14.3 MB (5.65%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -13,35 +13,34 @@ public:
             else
                 low=mid+1;
         }
-        return -1;
+        return low;
     }
-    int findPositionUpper(vector<int> &nums,int &target){
-        int low=0,high=nums.size()-1;
-        while(low<=high){
-            int mid=(low+high)/2;
-            if(nums[mid]==target)
-                if(mid!=nums.size()-1 && nums[mid+1]==target) low=mid+1;
-                else
-                    return mid;
-            else if(nums[mid]>target)
-                high=mid-1;
-            else
-                low=mid+1;
-        }
-        return -1;
-    }
+    // int findPositionUpper(vector<int> &nums,int &target){
+    //     int low=0,high=nums.size()-1;
+    //     while(low<=high){
+    //         int mid=(low+high)/2;
+    //         if(nums[mid]==target)
+    //             if(mid!=nums.size()-1 && nums[mid+1]==target) low=mid+1;
+    //             else
+    //                 return mid;
+    //         else if(nums[mid]>target)
+    //             high=mid-1;
+    //         else
+    //             low=mid+1;
+    //     }
+    //     return -1;
+    // }
     vector<int> searchRange(vector<int>& nums, int target) {
         vector<int> res;
-        // if(nums.size()==0) {
-        //      res.push_back(-1);
-        //     res.push_back(-1);
-        //     return res;
-        // }
         int lowerBound=findPositionLower(nums,target);
-        int upperBound=findPositionUpper(nums,target);
+        int upperBound=findPositionLower(nums,target+1);
+        if(lowerBound<nums.size() && nums[lowerBound]==target){
         res.push_back(lowerBound);
-        res.push_back(upperBound);
+        res.push_back(upperBound-1);
         return res;
+        }
+        else
+            return {-1,-1};
         
     }
 };
