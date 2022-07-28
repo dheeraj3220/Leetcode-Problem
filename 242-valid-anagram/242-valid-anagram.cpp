52d2d26b44a1f5f7babcbd1725095d05470a1adf
dheeraj3220 <76546604+dheeraj3220@users.noreply.github.com>
2022-07-28 18:39:50 +0530

    Time: 23 ms (22.29%), Space: 7.4 MB (7.79%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -3,13 +3,11 @@ public:
     bool isAnagram(string s, string t) {
         if(s.size()!=t.size()) return false;
         unordered_map<char,int> mapp;
-        for(auto iter: s)
-            mapp[iter]++;
+        for(auto iter: s) mapp[iter]++;
         for(auto iter: t){
             if(mapp[iter]==0) return false;
-            else{
+            else
                 mapp[iter]--;
-            }
         }
         return true;
     }
