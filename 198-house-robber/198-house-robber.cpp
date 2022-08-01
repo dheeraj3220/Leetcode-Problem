a6b31c5c5e66727e522c207bcce2e6f19d550627
dheeraj3220 <76546604+dheeraj3220@users.noreply.github.com>
2022-08-02 01:45:43 +0530

    Time: 3 ms (54.99%), Space: 7.8 MB (48.07%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,17 +1,6 @@
 class Solution {
 public:
-    // int maxRobbedMoney(vector<int> nums,int index,vector<int>&dp){
-    //     if(index>=nums.size()-1){
-    //         if(nums.size()-1==index) return nums[index];
-    //         return 0;
-    //     }
-    //     if(dp[index]!=-1) return dp[index];
-    //     int notRobbed=maxRobbedMoney(nums,index+1,dp);
-    //     int robbed=nums[index]+maxRobbedMoney(nums,index+2,dp);
-    //     return dp[index]=max(notRobbed,robbed);
-    // }
     int rob(vector<int>& nums) {
-        // vector<int> dp(nums.size()+1,-1);
         int prev=nums[0];
         int prev2=0;
         for(int i=1;i<nums.size();i++){
