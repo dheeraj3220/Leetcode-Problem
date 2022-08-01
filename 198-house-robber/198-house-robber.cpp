d3f00dec8eb0551af0bc7a4880570e73d8c42979
dheeraj3220 <76546604+dheeraj3220@users.noreply.github.com>
2022-08-02 01:42:25 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (25.02%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,17 +1,24 @@
 class Solution {
 public:
-    int maxRobbedMoney(vector<int> nums,int index,vector<int>&dp){
-        if(index>=nums.size()-1){
-            if(nums.size()-1==index) return nums[index];
-            return 0;
-        }
-        if(dp[index]!=-1) return dp[index];
-        int notRobbed=maxRobbedMoney(nums,index+1,dp);
-        int robbed=nums[index]+maxRobbedMoney(nums,index+2,dp);
-        return dp[index]=max(notRobbed,robbed);
-    }
+    // int maxRobbedMoney(vector<int> nums,int index,vector<int>&dp){
+    //     if(index>=nums.size()-1){
+    //         if(nums.size()-1==index) return nums[index];
+    //         return 0;
+    //     }
+    //     if(dp[index]!=-1) return dp[index];
+    //     int notRobbed=maxRobbedMoney(nums,index+1,dp);
+    //     int robbed=nums[index]+maxRobbedMoney(nums,index+2,dp);
+    //     return dp[index]=max(notRobbed,robbed);
+    // }
     int rob(vector<int>& nums) {
         vector<int> dp(nums.size()+1,-1);
-        return maxRobbedMoney(nums,0,dp);
+        dp[0]=nums[0];
+        for(int i=1;i<nums.size();i++){
+            int notRobbed=dp[i-1];
+            int robbed=nums[i];
+            if(i>1)robbed+=dp[i-2];
+            dp[i]=max(notRobbed,robbed);
+        }
+        return dp[nums.size()-1];
     }
 };
