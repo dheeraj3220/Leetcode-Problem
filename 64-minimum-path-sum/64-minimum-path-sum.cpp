8523c186098d2eed6c2d402aa2dd9eb9a0b9568c
dheeraj3220 <76546604+dheeraj3220@users.noreply.github.com>
2022-08-12 01:26:40 +0530

    Time: 4 ms (98.57%), Space: 10.2 MB (29.24%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -3,11 +3,13 @@
 //     int n=0,m=0;
 //     int helper(vector<vector<int>> grid,int i,int j,vector<vector<int>> dp){
 //         if(i==n-1 && j==m-1 ) return grid[i][j];
-//         if(i>=n || j>=m) return 1000000;
+//         if(i>=n || j>=m) return INT_MAX;
 //         if(dp[i][j]!=-1) return dp[i][j];
 //         int rightsum=helper(grid,i,j+1,dp);
 //         int downsum=helper(grid,i+1,j,dp);
-//         return dp[i][j]=grid[i][j]+min(rightsum,downsum);
+//         int ans=min(rightsum,downsum)+grid[i][j];
+//         dp[i][j]=ans;
+//         return ans;
 //     }
 //     int minPathSum(vector<vector<int>>& grid) {
 //         n=grid.size();
