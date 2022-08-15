9200fe9aeb066e0828dba499467e85fa4dd8fb71
dheeraj3220 <76546604+dheeraj3220@users.noreply.github.com>
2022-08-16 00:57:40 +0530

    Time: 1267 ms (23.07%), Space: 15.9 MB (12.28%) - LeetHub

--- 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.cpp
+++ 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
-     int helper(vector<vector<int>> & grid,int row,int col,vector<vector<int>>&dp ){
-        if(col<0 || col>=grid.size()) return 1e9;
-        if(row==grid.size()) return 0;
+     int helper(vector<vector<int>> & grid,int row,int col,vector<vector<int>>&dp ,int n){
+        if(col<0 || col>=n) return 1e9;
+        if(row==n) return 0;
         if(dp[row][col]!=-1) return dp[row][col];
         dp[row][col]=1e9;
-        for(int i=0;i<grid.size();i++){
-            if(i!=col) dp[row][col]=min(dp[row][col],helper(grid,row+1,i,dp));
+        for(int i=0;i<n;i++){
+            if(i!=col) dp[row][col]=min(dp[row][col],helper(grid,row+1,i,dp,n));
         }
         return dp[row][col]+=grid[row][col];
         
@@ -17,7 +17,7 @@ public:
         vector<vector<int>> dp(n,vector<int>(n,-1));
         int ans=1e9;
          for(int j=0;j<grid.size();j++){
-           ans=min(ans,helper(grid,0,j,dp));    
+           ans=min(ans,helper(grid,0,j,dp,n));    
         }
         return ans;
     }
