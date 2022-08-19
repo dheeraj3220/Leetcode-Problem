0e04e25a44c36609799a2678ff7af3543c100635
dheeraj3220 <76546604+dheeraj3220@users.noreply.github.com>
2022-08-19 19:53:41 +0530

    Time: 38 ms (90.23%), Space: 12.2 MB (97.02%) - LeetHub

---
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    void markIsland(vector<vector<char>>& grid, int i, int j){
+        if(i>=grid.size() || j>=grid[0].size() || i<0 || j<0)
+            return;
+        
+        if(grid[i][j]=='0')
+            return;
+        
+        grid[i][j]='0';
+        
+        markIsland(grid,i+1,j);
+        markIsland(grid,i-1,j);
+        markIsland(grid,i,j+1);
+        markIsland(grid,i,j-1);
+    }
+    int numIslands(vector<vector<char>>& grid) {
+        int count=0;
+        for(int i=0; i<grid.size(); i++){
+            for(int j=0; j<grid[0].size(); j++){
+                if(grid[i][j]=='1'){
+                    count++;
+                    markIsland(grid,i,j);
+                }
+            }
+        }
+        return count;
+    }
+};
