2ff90f6d5334a6e27a7a4d6ef40a66dd621d6462
dheeraj3220 <76546604+dheeraj3220@users.noreply.github.com>
2022-08-23 23:07:18 +0530

    Time: 112 ms (49.10%), Space: 61.6 MB (70.10%) - LeetHub

--- 334-increasing-triplet-subsequence/334-increasing-triplet-subsequence.cpp
+++ 334-increasing-triplet-subsequence/334-increasing-triplet-subsequence.cpp
@@ -5,15 +5,10 @@ public:
         int a,b;
         a=b=INT_MAX;
         for(int i=0;i<nums.size();i++){
-            if(nums[i]<=a){
-                a=nums[i];
-            }
-            else if(nums[i]<=b){
-                b=nums[i];
-            }
-            else{
+            if(nums[i]<=a) a=nums[i];
+            else if(nums[i]<=b) b=nums[i];
+            else
                 return true;
-            }
         }
         return false;
     }
