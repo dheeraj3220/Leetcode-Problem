d4084fb9ec422a0d076b55d0ef1cb7dcc0eb4699
dheeraj3220 <76546604+dheeraj3220@users.noreply.github.com>
2022-08-29 03:29:02 +0530

    Time: 93 ms (78.81%), Space: 15.8 MB (33.31%) - LeetHub

--- 1473-paint-house-iii/1473-paint-house-iii.cpp
+++ 1473-paint-house-iii/1473-paint-house-iii.cpp
@@ -7,7 +7,7 @@ public:
         if(target==0) return 0;
         else return 1e9;
         }
-        if(dp[houseIndex][prevColor+1][target]!=-1) return dp[houseIndex][prevColor+1][target];
+        if(prevColor!=-1 && dp[houseIndex][prevColor][target]!=-1) return dp[houseIndex][prevColor][target];
         int colorCost=1e9;
         if(houses[houseIndex]==0){
             for(int i=0;i<cost[0].size();i++){
@@ -19,7 +19,8 @@ public:
             if(prevColor==houses[houseIndex]-1) colorCost=min(colorCost,helper(houses,cost,target,houseIndex+1,prevColor));
                 else colorCost=min(colorCost,helper(houses,cost,target-1,houseIndex+1,houses[houseIndex]-1));
         }
-        return dp[houseIndex][prevColor+1][target]=colorCost;
+        if(prevColor==-1) return colorCost;
+        return dp[houseIndex][prevColor][target]=colorCost;
         
     }
     int minCost(vector<int>& houses, vector<vector<int>>& cost, int m, int n, int target) {
