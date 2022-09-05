e814e8d26155d4ccb2400d8edb46c2f40e02da7b
dheeraj3220 <76546604+dheeraj3220@users.noreply.github.com>
2022-09-05 09:47:23 +0530

    Time: 3 ms (93.89%), Space: 12 MB (93.87%) - LeetHub

--- 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
+++ 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -1,28 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-   unordered_map<int, unordered_map<int, multiset<int>>> m;
-void traverse(TreeNode* r, int x, int y) {
-  if (r != nullptr) {
-    m[x][y].insert(r->val);
-    traverse(r->left, x - 1, y + 1);
-    traverse(r->right, x + 1, y + 1);
-  }
-}
-vector<vector<int>> verticalTraversal(TreeNode* r) {
-  vector<vector<int>> res;
-  traverse(r, 0, 0);
-  for (int x = -999; x < 1000; ++x) {
-    auto it = m.find(x);
-    if (it != end(m)) {
-      res.push_back(vector<int>());
-      for (int y = 0; y < 1000; ++y) {
-        auto ity = it->second.find(y);
-        if (ity != end(it->second)) {
-          res.back().insert(end(res.back()), begin(ity->second), end(ity->second));
-        }
-      }
+       void helper(TreeNode*root,int order,int level,priority_queue <pair<int,pair<int,int>>, vector<pair<int,pair<int,int>>>, greater<pair<int,pair<int,int>>> >&pq){
+        if(root==NULL) return ;
+        pair<int,pair<int,int>> p;
+        p.first=order;
+        p.second.first=level;
+        p.second.second=root->val;
+        pq.push(p);
+        helper(root->left,order-1,level+1,pq);
+        helper(root->right,order+1,level+1,pq);
     }
-  }
-  return res;
-}
+    vector<vector<int>> verticalTraversal(TreeNode* root) {
+        vector<vector<int>> res;
+        priority_queue <pair<int,pair<int,int>>, vector<pair<int,pair<int,int>>>, greater<pair<int,pair<int,int>>> > pq;
+        helper(root,0,0,pq);
+        vector<int> temp;
+        int prev=0;
+        while(!pq.empty()){
+            if(temp.empty() || prev==pq.top().first){
+                int t=pq.top().second.second;
+                temp.push_back(t);
+                prev=pq.top().first;
+                pq.pop();
+            }
+            else{
+                res.push_back(temp);
+                temp.clear();
+            }
+        }
+        res.push_back(temp);
+        return res;
+        
+    }
 };
