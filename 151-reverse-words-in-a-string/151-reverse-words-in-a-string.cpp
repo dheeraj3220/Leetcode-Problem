976df471734c3b322e452ae325e89374b4b14d81
dheeraj3220 <76546604+dheeraj3220@users.noreply.github.com>
2022-09-09 20:35:16 +0530

    Time: 11 ms (47.87%), Space: 7.2 MB (84.00%) - LeetHub

--- 151-reverse-words-in-a-string/151-reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/151-reverse-words-in-a-string.cpp
@@ -4,22 +4,18 @@ public:
         int j=s.size()-1;
         while(s[j]==' ')j--;
         string res="";
-        int i=0;
-        int charCount=0;
+        int i=0,charCount=0;
         while(i<=j){
             if(s[i]==' ' && charCount==0) i++;
             else if(s[i]==' ' && charCount==1) {
-                res+=s[i];
-                i++;
+                res+=s[i++];
                 charCount=0;
             }
             else{
-                res+=s[i];
-                i++;
+                res+=s[i++];
                 charCount=1;
             }
         }
-        // return res;
         reverse(res.begin(),res.end());
         int start=0;
         for(int i=0;i<=res.size();i++){
