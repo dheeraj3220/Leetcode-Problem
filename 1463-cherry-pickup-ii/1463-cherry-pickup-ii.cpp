96237a5c7c1305d9c2761c600732cd9c4aa0acd5
dheeraj3220 <76546604+dheeraj3220@users.noreply.github.com>
2022-09-23 01:48:47 +0530

    Time: 302 ms (24.02%), Space: 17 MB (49.80%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -1,25 +1,22 @@
 class Solution {
 public:
-    int helper(vector<vector<int>>&grid,int i,int j1,int j2,vector<vector<vector<int>>>&dp){
-        if(j1<0 || j1>=grid[0].size() || j2<0 || j2>=grid[0].size()) return -10000000;
-        if(i==grid.size()-1) {
-            if(j1==j2) return grid[i][j1];
-            else return grid[i][j1]+grid[i][j2];
-        }
-        if(dp[i][j1][j2]!=-1) return dp[i][j1][j2];
-        int maxi=0;
+    int helper(vector<vector<int>>& grid,int row,int col1,int col2 ,vector<vector<vector<int>>>& dp){
+        if(col1<0 || col1>=grid[0].size()||col2<0 || col2>=grid[0].size()) return -10000000;
+        if(row==grid.size()) return 0;
+        if(dp[row][col1][col2]!=-1) return dp[row][col1][col2];
+        int cherry=0;
         for(int m1=-1;m1<=1;m1++){
             for(int m2=-1;m2<=1;m2++){
-                if(j1==j2) maxi=max(maxi,grid[i][j1]+helper(grid,i+1,j1+m1,j2+m2,dp));
-                else maxi=max(maxi,grid[i][j1]+grid[i][j2]+helper(grid,i+1,j1+m1,j2+m2,dp));
+               if(col2==col1) cherry=max(cherry,grid[row][col1]+helper(grid,row+1,col1+m1,col2+m2,dp));
+                else cherry=max(cherry,grid[row][col1]+grid[row][col2]+helper(grid,row+1,col1+m1,col2+m2,dp));
             }
         }
-        return dp[i][j1][j2]=maxi;
+        return dp[row][col1][col2]=cherry;
     }
     int cherryPickup(vector<vector<int>>& grid) {
-        int row=grid.size();
-        int col=grid[0].size();
-        vector<vector<vector<int>>> dp(row,vector<vector<int>>(col,vector<int>(col,-1)));
-        return helper(grid,0,0,col-1,dp); 
+        int n=grid.size();
+        int m=grid[0].size();
+        vector<vector<vector<int>>> dp(n,vector<vector<int>>(m,vector<int>(m,-1)));
+        return helper(grid,0,0,m-1,dp);
     }
 };
