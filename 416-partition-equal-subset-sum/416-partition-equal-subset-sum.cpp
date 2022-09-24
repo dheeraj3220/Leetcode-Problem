4a61e972eaa002bc8227cfcbf637e0e9ec16de93
dheeraj3220 <76546604+dheeraj3220@users.noreply.github.com>
2022-09-24 15:27:21 +0530

    Time: 516 ms (32.63%), Space: 114.2 MB (13.13%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -3,8 +3,8 @@ public:
     int helper(vector<int> &nums,int index,int k,vector<vector<int>>&dp){
         if(k==0) return 1;
         if(nums.size()==index){
-          if(k==0) return 1;
-          else
+          // if(k==0) return 1;
+          // else
             return 0;  
         } 
         if(dp[index][k]!=-1) return dp[index][k];
@@ -14,7 +14,7 @@ public:
         return dp[index][k];
         
         // dp[index][k]=helper(nums,index+1,k-nums[index],dp);
-        // if(dp[index][k]==1) return 1;
+        // if(dp[index][k]==1) return dp[index][k];
         // return dp[index][k]=helper(nums,index+1,k,dp);
     }
     bool canPartition(vector<int>& nums) {
