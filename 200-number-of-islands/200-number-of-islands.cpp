3bf9b4e345fdd12a8049b80f867769048105f707
dheeraj3220 <76546604+dheeraj3220@users.noreply.github.com>
2022-09-29 02:30:38 +0530

    Time: 97 ms (12.97%), Space: 12.3 MB (82.64%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,31 +1,31 @@
 class Solution {
 public:
-    void bfs(vector<vector<char>>&grid,int i,int j){
-        queue<pair<int,int>> que;
-        que.push({i,j});
-        grid[i][j]='0';
-        while(!que.empty()){
-            int x=que.front().first;
-            int y=que.front().second;
-            que.pop();
-            if(x+1<grid.size() && grid[x+1][y]=='1'){ 
-                que.push({x+1,y});
-                grid[x+1][y]='0';
-            }
-            if(x-1>=0 && grid[x-1][y]=='1'){
-                que.push({x-1,y});
-                grid[x-1][y]='0';    
-            }
-            if(y+1<grid[0].size() && grid[x][y+1]=='1') {
-                que.push({x,y+1});
-                grid[x][y+1]='0';
-            }
-            if(y-1>=0 && grid[x][y-1]=='1'){
-                que.push({x,y-1});
-                grid[x][y-1]='0';
-            }
-        }
-    }
+    // void bfs(vector<vector<char>>&grid,int i,int j){
+    //     queue<pair<int,int>> que;
+    //     que.push({i,j});
+    //     grid[i][j]='0';
+    //     while(!que.empty()){
+    //         int x=que.front().first;
+    //         int y=que.front().second;
+    //         que.pop();
+    //         if(x+1<grid.size() && grid[x+1][y]=='1'){ 
+    //             que.push({x+1,y});
+    //             grid[x+1][y]='0';
+    //         }
+    //         if(x-1>=0 && grid[x-1][y]=='1'){
+    //             que.push({x-1,y});
+    //             grid[x-1][y]='0';    
+    //         }
+    //         if(y+1<grid[0].size() && grid[x][y+1]=='1') {
+    //             que.push({x,y+1});
+    //             grid[x][y+1]='0';
+    //         }
+    //         if(y-1>=0 && grid[x][y-1]=='1'){
+    //             que.push({x,y-1});
+    //             grid[x][y-1]='0';
+    //         }
+    //     }
+    // }
     void dfs(vector<vector<char>>&grid,int i,int j){
         if(i<0 || j<0 || i>=grid.size() || j>=grid[0].size() || grid[i][j]=='0') return ;
         grid[i][j]='0';
@@ -34,6 +34,7 @@ public:
         dfs(grid,i,j+1);
         dfs(grid,i,j-1);
     }
+    
     int numIslands(vector<vector<char>>& grid) {
         int count=0;
         for(int i=0;i<grid.size();i++){
