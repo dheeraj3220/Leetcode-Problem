c599acb4a32c8443b33c435bac8b71151ba6921a
dheeraj3220 <76546604+dheeraj3220@users.noreply.github.com>
2022-09-29 00:46:43 +0530

    Time: 55 ms (27.62%), Space: 13.7 MB (89.06%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.cpp
+++ 547-number-of-provinces/547-number-of-provinces.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
-    void bfs(vector<vector<int>>&isConnected,vector<int>&visited,int node){
+    
+        /*************Breadth First Search***************/
+    
+    
+    /* void bfs(vector<vector<int>>&isConnected,vector<int>&visited,int node){
         queue<int> que;
         que.push(node);
         visited[node]=1;
@@ -14,14 +18,25 @@ public:
                 }
             }
         }
+    } */
+    
+     /***************Depth First Search*****************/
+    void dfs(vector<vector<int>>&isConnected,vector<int>&visited,int node){
+        visited[node]=1;
+        for(int i=0;i<isConnected.size();i++){
+            if(isConnected[node][i]==1 && !visited[i]) dfs(isConnected,visited,i);
+        }
     }
+    
+    
     int findCircleNum(vector<vector<int>>& isConnected) {
         vector<int> visited(isConnected.size(),0);
         int count=0;
         for(int i=0;i<isConnected.size();i++){
             if(!visited[i]){
                 count++;
-                bfs(isConnected,visited,i);
+                // bfs(isConnected,visited,i);
+                dfs(isConnected,visited,i);
             }
         }
         return count;
