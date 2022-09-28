25b50668d996c9587b65e66b93e0c3ba4067242b
dheeraj3220 <76546604+dheeraj3220@users.noreply.github.com>
2022-09-29 02:25:21 +0530

    Time: 98 ms (12.43%), Space: 18.2 MB (17.21%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,26 +1,38 @@
 class Solution {
 public:
-    void markIsland(vector<vector<char>>& grid, int i, int j){
-        if(i>=grid.size() || j>=grid[0].size() || i<0 || j<0 || grid[i][j]=='0')
-            return;
-        
-        // if(grid[i][j]=='0')
-        //     return;
-        
+    void bfs(vector<vector<char>>&grid,int i,int j){
+        queue<pair<int,int>> que;
+        que.push({i,j});
         grid[i][j]='0';
-        
-        markIsland(grid,i+1,j);
-        markIsland(grid,i-1,j);
-        markIsland(grid,i,j+1);
-        markIsland(grid,i,j-1);
+        while(!que.empty()){
+            int x=que.front().first;
+            int y=que.front().second;
+            que.pop();
+            if(x+1<grid.size() && grid[x+1][y]=='1'){ 
+                que.push({x+1,y});
+                grid[x+1][y]='0';
+            }
+            if(x-1>=0 && grid[x-1][y]=='1'){
+                que.push({x-1,y});
+                grid[x-1][y]='0';    
+            }
+            if(y+1<grid[0].size() && grid[x][y+1]=='1') {
+                que.push({x,y+1});
+                grid[x][y+1]='0';
+            }
+            if(y-1>=0 && grid[x][y-1]=='1'){
+                que.push({x,y-1});
+                grid[x][y-1]='0';
+            }
+        }
     }
     int numIslands(vector<vector<char>>& grid) {
         int count=0;
-        for(int i=0; i<grid.size(); i++){
-            for(int j=0; j<grid[0].size(); j++){
+        for(int i=0;i<grid.size();i++){
+            for(int j=0;j<grid[0].size();j++){
                 if(grid[i][j]=='1'){
                     count++;
-                    markIsland(grid,i,j);
+                    bfs(grid,i,j);
                 }
             }
         }
