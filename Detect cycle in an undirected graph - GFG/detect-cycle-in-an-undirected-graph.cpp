90e99e338c29dee76872efc4f6f491c79c34a286
dheeraj3220 <76546604+dheeraj3220@users.noreply.github.com>
2022-10-23 21:47:16 +0530

    Added solution - LeetHub

--- Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.cpp
+++ Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.cpp
@@ -6,31 +6,42 @@ using namespace std;
 class Solution {
   public:
     // Function to detect cycle in an undirected graph.
-    bool bfs(vector<int> & visited,vector<int> adj[],int start){
-        queue<pair<int,int>> que;
-        que.push({start,-1});
+    // bool bfs(vector<int> & visited,vector<int> adj[],int start){
+    //     queue<pair<int,int>> que;
+    //     que.push({start,-1});
+    //     visited[start]=1;
+    //     while(!que.empty()){
+    //         int curNode=que.front().first;
+    //         int prevNode=que.front().second;
+    //         que.pop();
+    //         for(int i=0;i<adj[curNode].size();i++){
+    //             if(adj[curNode][i]!=prevNode && visited[adj[curNode][i]]==1)
+    //                 return true;
+    //             else if(adj[curNode][i]!=prevNode){
+    //                 visited[adj[curNode][i]]=1;
+    //                 que.push({adj[curNode][i],curNode});
+    //             }
+    //         }
+    //     }
+    //     return false;
+    // }
+    
+    ////////////********* DFS *************\\\\\\\\\\\\\\\\\\\\
+    
+    bool dfs(vector<int> &visited,vector<int> adj[],int start,int prev){
+        if(visited[start]==1) return true;
         visited[start]=1;
-        while(!que.empty()){
-            int curNode=que.front().first;
-            int prevNode=que.front().second;
-            que.pop();
-            for(auto node : adj[curNode]){
-                if(visited[node]==0){
-                    visited[node]=1;
-                    que.push({node,curNode});
-                }
-                else if(node!=prevNode){
-                    return true;
-                }
-            }
+        for(auto node : adj[start]){
+            if(prev!=node && dfs(visited,adj,node,start)) return true;
         }
-        return false;
+        return false; 
     }
+    
     bool isCycle(int V, vector<int> adj[]) {
         vector<int> visited(V,0);
         for(int i=0;i<V;i++){
             if(!visited[i]){
-                if(bfs(visited,adj,i)) return true;
+                if(dfs(visited,adj,i,-1)==true) return true;
             }
         }
         return false;
