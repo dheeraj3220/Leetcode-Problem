0e16cef188bc3e64290050f3885d2cac0a223cd3
dheeraj3220 <76546604+dheeraj3220@users.noreply.github.com>
2022-10-25 23:50:49 +0530

    Time: 56 ms (46.98%), Space: 13.5 MB (84.56%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -1,19 +1,29 @@
 class Solution {
 public:
-    bool bfs(vector<vector<int>> &graph,int start,vector<int>&vis){
-        queue<int> que;
-        que.push(start);
-        vis[start]=0;
-        while(!que.empty()){
-            int curNode=que.front();
-            que.pop();
-            for(auto node : graph[curNode]){
-                if(vis[node]!=-1 && vis[node]==vis[curNode]) return false;
-                else if(vis[node]==-1){
-                    vis[node]=vis[curNode]==0?1:0;
-                    que.push(node);
-                }
+    // bool bfs(vector<vector<int>> &graph,int start,vector<int>&vis){
+    //     queue<int> que;
+    //     que.push(start);
+    //     vis[start]=0;
+    //     while(!que.empty()){
+    //         int curNode=que.front();
+    //         que.pop();
+    //         for(auto node : graph[curNode]){
+    //             if(vis[node]!=-1 && vis[node]==vis[curNode]) return false;
+    //             else if(vis[node]==-1){
+    //                 vis[node]=vis[curNode]==0?1:0;
+    //                 que.push(node);
+    //             }
+    //         }
+    //     }
+    //     return true;
+    // }
+    bool dfs(vector<vector<int>>&graph,int start,int color,vector<int>&vis){
+        vis[start]=color;
+        for(auto node : graph[start]){
+            if(vis[node]==-1){
+                if(dfs(graph,node,!color,vis)==false) return false;
             }
+            else if(vis[start]==vis[node]) return false;
         }
         return true;
     }
@@ -22,7 +32,8 @@ public:
         vector<int> vis(n,-1);
         for(int i=0;i<n;i++){
             if(vis[i]==-1){
-                if(!bfs(graph,i,vis)) return false;
+                // if(!bfs(graph,i,vis)) return false;
+                if(!dfs(graph,i,0,vis)) return false;
             } 
         }
         return true;        
