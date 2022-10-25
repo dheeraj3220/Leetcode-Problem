143803f1c5f63ce84c32def53669b7016969d5ff
dheeraj3220 <76546604+dheeraj3220@users.noreply.github.com>
2022-10-25 14:37:37 +0530

    Added solution - LeetHub

---
+++ Number Of Enclaves - GFG/number-of-enclaves.cpp
@@ -0,0 +1,68 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+public:
+     void dfs(vector<vector<int>> & board,vector<vector<int>>& visited,int x,int y){
+        
+        int m=board.size();
+        int n=board[0].size();
+        visited[x][y]=1;
+        board[x][y]=0;
+        if(x+1<m && board[x+1][y]!=0 && !visited[x+1][y])dfs(board,visited,x+1,y);
+        if(x-1>=0 && board[x-1][y]!=0 &&  !visited[x-1][y])dfs(board,visited,x-1,y);
+        if(y+1<n && board[x][y+1]!=0 && !visited[x][y+1])dfs(board,visited,x,y+1);
+        if(y-1>=0 && board[x][y-1]!=0 && !visited[x][y-1])dfs(board,visited,x,y-1);
+        board[x][y]=1;
+    }
+    
+    
+    int numberOfEnclaves(vector<vector<int>>& board) {
+        int count=0;
+         int m=board.size();
+        int n=board[0].size();
+        vector<vector<int>> visited(m,vector<int>(n,0));
+        for(int i=0;i<n;i++){
+            if(board[0][i]==1 && !visited[0][i]) dfs(board,visited,0,i);
+            if(board[m-1][i]==1 && !visited[m-1][i]) dfs(board,visited,m-1,i);
+        }
+        for(int i=0;i<m;i++){
+            if(board[i][0]==1 && !visited[i][0]) dfs(board,visited,i,0);
+            if(board[i][n-1]==1 && !visited[i][n-1]) dfs(board,visited,i,n-1);
+        }
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(board[i][j]==1 && !visited[i][j]) count++;
+            }
+        }
+        return count;
+        
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m;
+        cin >> n >> m;
+        vector<vector<int>> grid(n, vector<int>(m));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                cin >> grid[i][j];
+            }
+        }
+        Solution obj;
+        cout << obj.numberOfEnclaves(grid) << endl;
+    }
+}
+// } Driver Code Ends
