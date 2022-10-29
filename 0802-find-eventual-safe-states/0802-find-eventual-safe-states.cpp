dbd9c0d99f4be8de7432936edabec6570a39471e
dheeraj3220 <76546604+dheeraj3220@users.noreply.github.com>
2022-10-30 00:05:42 +0530

    Time: 278 ms (72.14%), Space: 58.7 MB (39.54%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -1,28 +1,59 @@
 class Solution {
 public:
     private: 
-    bool dfs(vector<vector<int>>& graph,vector<int>&vis,int start,vector<int>&res){
-        vis[start]=2;
-        for(auto node : graph[start]){
-            if(!vis[node]){
-                if(dfs(graph,vis,node,res)) return true;
-            }
-            else if(vis[node]==2) return true;
-        }
-        res.push_back(start);
-        vis[start]=1;
-        return false;
-    }  
+    // bool dfs(vector<vector<int>>& graph,vector<int>&vis,int start,vector<int>&res){
+    //     vis[start]=2;
+    //     for(auto node : graph[start]){
+    //         if(!vis[node]){
+    //             if(dfs(graph,vis,node,res)) return true;
+    //         }
+    //         else if(vis[node]==2) return true;
+    //     }
+    //     res.push_back(start);
+    //     vis[start]=1;
+    //     return false;
+    // }  
   public:
-    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-        // code here
-        vector<int> vis(graph.size(),0);
-        vector<int> res;
-        for(int i=0;i<graph.size();i++){
-            if(!vis[i]){
-                dfs(graph,vis,i,res);
+    vector<int> eventualSafeNodes(vector<vector<int>>& adj) {
+        // vector<int> vis(graph.size(),0);
+        // vector<int> res;
+        // for(int i=0;i<graph.size();i++){
+        //     if(!vis[i]){
+        //         dfs(graph,vis,i,res);
+        //     }
+        // }
+        // sort(res.begin(),res.end());
+        // return res;
+        int V=adj.size();
+        vector<int> newAdj[V];
+        for(int i=0;i<V;i++){
+            for(auto node : adj[i]){
+                newAdj[node].push_back(i);
             }
         }
+        vector<int> inDegree(V,0);
+        for(int i=0;i<V;i++){
+            for(auto node: newAdj[i]){
+                inDegree[node]++;
+            }
+        }
+        queue<int> que;
+        vector<int> res;
+        for(int i=0;i<V;i++) if(inDegree[i]==0) que.push(i);
+        while(!que.empty()){
+            int curNode=que.front();
+            res.push_back(curNode);
+            que.pop();
+            for(auto node : newAdj[curNode]){
+                inDegree[node]--;
+                if(inDegree[node]==0) que.push(node);
+            }
+        }
+        // for(int i=0;i<V;i++){
+        //     if(!vis[i]){
+        //         dfs(adj,vis,i,res);
+        //     }
+        // }
         sort(res.begin(),res.end());
         return res;
     }
