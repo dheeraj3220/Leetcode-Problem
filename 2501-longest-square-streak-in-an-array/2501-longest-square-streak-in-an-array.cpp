f90ed5cb9e270385278395ff71ec88f64938dc49
dheeraj3220 <76546604+dheeraj3220@users.noreply.github.com>
2022-12-11 11:34:37 +0530

    Time: 326 ms (64.29%), Space: 107.4 MB (7.14%) - LeetHub

--- 2501-longest-square-streak-in-an-array/2501-longest-square-streak-in-an-array.cpp
+++ 2501-longest-square-streak-in-an-array/2501-longest-square-streak-in-an-array.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     int longestSquareStreak(vector<int>& nums) {
-        set<long long> mapp;
+        set<long long> mySet;
         for(auto num : nums) {
-            mapp.insert(num);
+            mySet.insert(num);
         }
-        long long maxCount=0;
-        for(auto i=mapp.begin();i!=mapp.end();i=mapp.begin()){
-            long long count=1;
+        int maxCount=0;
+        for(auto i=mySet.begin();i!=mySet.end();i=mySet.begin()){
+            int count=1;
             long long ft=*i;
             if(ft>320) break;
-            while(mapp.count(ft*ft)==1){
+            while(mySet.count(ft*ft)==1){
                 count++;
-                mapp.erase(ft);
+                mySet.erase(ft);
                 ft=ft*ft;
             }
-            mapp.erase(ft);
+            mySet.erase(ft);
             maxCount=max(count,maxCount);
         }
         if(maxCount==1 || maxCount==0) return -1;
