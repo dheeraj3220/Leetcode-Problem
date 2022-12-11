126bf11c3c0db2c9651c74a1e076b02811e11e7f
dheeraj3220 <76546604+dheeraj3220@users.noreply.github.com>
2022-12-11 15:26:59 +0530

    Time: 147 ms (100.00%), Space: 65.3 MB (55.56%) - LeetHub

--- 2498-frog-jump-ii/2498-frog-jump-ii.cpp
+++ 2498-frog-jump-ii/2498-frog-jump-ii.cpp
@@ -1,9 +1,10 @@
 class Solution {
 public:
-   int maxJump(vector<int>& st) {
-    int res = st[1] - st[0];
-    for (int i = 1; i < st.size() - 1; ++i)
-        res = max(res, st[i + 1] - st[i - 1]);
-    return res;
-}
+    int maxJump(vector<int>& stones) {
+        int diff=stones[1]-stones[0];
+        for(int i=1;i<stones.size()-1;i++){
+            diff=max(diff,stones[i+1]-stones[i-1]);
+        }
+        return diff;
+    }
 };
