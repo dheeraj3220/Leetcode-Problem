2ac3819a8756d09210fb13261bbad05d9d166246
dheeraj3220 <76546604+dheeraj3220@users.noreply.github.com>
2022-12-18 10:25:11 +0530

    Time: 2651 ms (12.50%), Space: 245.2 MB (12.50%) - LeetHub

--- 2506-count-pairs-of-similar-strings/2506-count-pairs-of-similar-strings.cpp
+++ 2506-count-pairs-of-similar-strings/2506-count-pairs-of-similar-strings.cpp
@@ -5,12 +5,18 @@ public:
         int count=0;
         for(int i=0;i<words.size();i++){
             unordered_map<char,int> mapp;
-            for(int j=0;j<words[i].size();j++) mapp[words[i][j]]=1;
+            for(int j=0;j<words[i].size();j++) mapp[words[i][j]]++;
             for(int j=i+1;j<words.size();j++){
                 unordered_map<char,int> mappi;
-                for(auto it : words[j]) mappi[it]=1;
-                
-                if(mapp==mappi) count++;
+                for(auto it : words[j]) mappi[it]++;
+                bool flag=true;
+                for(char ch ='a';ch<='z';ch++){
+                    if(mapp.count(ch)==1 && mappi.count(ch)==0 || mapp.count(ch)==0 && mappi.count(ch)==1){
+                        flag=false;
+                        break;
+                    }
+                };
+                if(flag) count++;
             }
         }
         return count;
